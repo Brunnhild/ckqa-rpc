941e3b28b61c7237bb35e968d5fc2c97d71b3b16
Brunnhild <2914095626@qq.com>
2022-04-10 16:31:34 +0800

    feat: kb source and new rpc api
    substitue gstore with es
    add support for text based QA
    allow selection of kb source

--- ckqa.thrift
+++ ckqa.thrift
@@ -14,9 +14,10 @@ struct Tuple {
 
 service CKQA
 {
-    list<Result> getMaskResult(1:string query);
+    list<Result> getMaskResult(1:string query, 2:bool includeNone, 3:bool includeCSKG);
     list<Result> getSpanResult(1:string query);
     list<Result> getMaskWordResult(1:string query);
+    list<Result> getTextQaResult(1:string query, 2:string text);
 
     list<Tuple> getExtraction(1:string query);
 }
